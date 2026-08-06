ae7a710db1a39f3ff063e2876687a6bffead17c1
jiangtengqiao <jiangtengqiao@users.noreply.github.com>
2026-08-06 13:26:38 +0800

    v1.1.1: 预填Render地址 + gradle wrapper
    - Android BASE_URL 预填 Render 托管地址
    - 新增 gradle-wrapper.properties

--- android/app/src/main/java/com/novel/reader/Constants.kt
+++ android/app/src/main/java/com/novel/reader/Constants.kt
@@ -4,8 +4,8 @@ package com.novel.reader
  * 全局常量
  */
 object Constants {
-    /** 后端服务地址 - 部署时替换为实际服务器地址 */
-    const val BASE_URL = "http://10.0.2.2:8000/"
+    /** 后端服务地址 - Render 免费托管地址 */
+    const val BASE_URL = "https://moyue-novel-api.onrender.com/"
 
     /** 当前应用版本名 */
     const val APP_VERSION = "1.1.0"
